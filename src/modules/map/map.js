4830ff31b93eb04a01e08c96d7674e971394b244
MisterTigre <mrtigre360@gmail.com>
2025-09-29 17:52:20 +0200

    add markers and polygons

---
+++ src/modules/map/map.js
@@ -0,0 +1,92 @@
+var map
+var popup
+var custom_icons = {}
+
+
+async function createMap() {
+    const response = await fetch('map.json');
+    const json = await response.json();
+    console.log(json)
+
+    // Create the base map
+    map = L.map('map').setView([json.latitude, json.longitude], json.zoom)
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map)
+    popup = L.popup()
+
+    // Desactive all movements if you want it
+    if (json.dragable != true){
+        map.removeControl(map.zoomControl);
+        map.dragging.disable();
+        map.touchZoom.disable();
+        map.doubleClickZoom.disable();
+        map.scrollWheelZoom.disable();
+        map.boxZoom.disable();
+        map.keyboard.disable();
+    }
+
+    // create custom icons
+    json.custom_icons.forEach(ci => {
+        custom_icons[ci.name] = L.icon({
+            iconUrl: ci.iconUrl,  
+            iconSize: ci.iconSize,              
+            iconAnchor: ci.iconAnchor,            
+            popupAnchor: ci.popupAnchor           
+        })
+    })
+
+
+    // Add markers to the map
+    json.markers.forEach(m => {
+        var icon = L.Icon.Default.prototype
+        if (m.icon in custom_icons){
+            icon = custom_icons[m.icon]
+        }else if (m.icon != "default"){
+            console.error(`The icon "${m.icon}" has not been declared`)
+        }
+        var new_marker = L.marker([m.latitude, m.longitude], {icon: icon}).addTo(map)
+        if (m.popup_text != ""){
+            new_marker.bindPopup(m.popup_text)
+        }
+    })
+
+
+    // Add polygons to the map
+    json.polygons.forEach(p => {
+        var new_polygon = L.polygon(p.points, {
+            color: p.color,
+            fillColor: p.fillColor,
+            opacity: p.opacity,        // contour invisible par défaut
+            fillOpacity: p.fillOpacity     // remplissage invisible par défaut
+        }).addTo(map);
+
+        if (p.popup_text != "") {
+            new_polygon.bindPopup(p.popup_text)
+        }
+
+        if (p.hover_view){
+            new_polygon.setStyle({ opacity: 0, fillOpacity: 0 })
+            new_polygon.on('mouseover', function () {
+                this.setStyle({ opacity: p.opacity, fillOpacity: p.fillOpacity })
+            });
+
+            new_polygon.on('mouseout', function () {
+                this.setStyle({ opacity: 0, fillOpacity: 0 })
+            });
+        }
+    });
+}
+
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map)
+}
+
+
+createMap()
+map.on('click', onMapClick)
